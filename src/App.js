a52b8d0a2e7323b5e592f46110215aa1418be7e5
AbdurrahmanTalha <abdurrahmantalha331@gmail.com>
2022-04-17 10:38:17 +0600

    finished with notfound

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
-// import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Blogs from './Components/Blogs/Blogs';
 import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import Navbar from './Components/Navbar/Navbar';
+import Notfound from './Components/Notfound/Notfound';
 import SignUp from './Components/SignUp/SignUp';
 
 function App() {
   return (
-  <div>
-    <Navbar></Navbar>
-    <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
-      <Route path="/signup" element={<SignUp></SignUp>}></Route>
-      <Route path="/login" element={<Login></Login>}></Route>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
-      </Routes> 
+        <Route path="*" element={<Notfound></Notfound>}></Route>
+      </Routes>
       <Footer></Footer>
-  </div>)
+    </div>)
 
 
 }
---
+++ src/Components/Notfound/Notfound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Notfound = () => {
+    return (
+        <div className='d-flex align-items-center justify-content-center'>
+            <img src="https://www.lifewire.com/thmb/zZnzOJ4GL4YQEa6E8c7-hSpx7Nc=/2667x2000/smart/filters:no_upscale()/404-not-found-error-explained-2622936-Final-fde7be1b7e2e499c9f039d97183e7f52.jpg" alt="not found img" className='img-fluid w-50'/>
+        </div>
+    );
+};
+
+export default Notfound;
